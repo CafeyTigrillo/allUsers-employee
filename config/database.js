371d4f7a63324b5016ba422c8623ca345704c52f
Edgar <epgarzonr@uce.edu.ec>
2025-02-02 09:32:58 -0500

    feat: use environment variables for database connection

--- config/database.js
+++ config/database.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '12345', 
-    database: 'franchise'
+const db = mysql.createConnection({ host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to database');
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to database');
 });
 
 module.exports = db;
