fd6df7e35c34b26e02b491c85799ebbaeea9f2bb
Edgar <epgarzonr@uce.edu.ec>
2025-02-02 09:02:48 -0500

    feat: set up Express server with MySQL connection and user routes

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql2');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', 
+  database: 'franchise'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to database');
+});
+
+global.db = db;
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
